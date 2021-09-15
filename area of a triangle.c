3aaf2560e1f59e7a2d112284dbb9a5f06a8b7b3b
Pokemon <90720610+xpokemon@users.noreply.github.com>
2021-09-16 00:28:07 +0600

    Add files via upload

---
+++ area of a triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    float height,base,area;
+    printf("Enter Height: ");
+    scanf("%f",&height);
+     printf("Enter Base: ");
+    scanf("%f",&base);
+    area=0.5*height*base;
+    printf("Area of a triangle is: %0.2f",area);
+
+}
