4a65984bfba27369ae164d969c871b3fb3755ceb
Pokemon <90720610+xpokemon@users.noreply.github.com>
2021-09-16 00:37:04 +0600

    Add files via upload

---
+++ 3 length area of a Triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    double a,b,c,s,area;
+    printf("Enter 3 values: ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("Area is: %0.2lf",area);
+
+}
